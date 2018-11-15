5db2f5f9fa216a8eabec7785296e0130cdc57d2f
Cindy Colin <c.colin@interface3.be>
2018-11-15 12:05:27 +0100

    ajout liens pages NL FR EN dans footer accueil_FR

--- accueil_FR.php
+++ accueil_FR.php
@@ -104,5 +104,14 @@
             </script> -->
 
 	
-	<footer><p> &copy; Bénédicte Regnier, Floriane Jacques, Sarah Gueye, Cindy Colin - Interface 3</p></footer>
+	<footer>
+	    <p> &copy; Bénédicte Regnier, Floriane Jacques, Sarah Gueye, Cindy Colin - Interface 3</p>
+	    <nav>
+	        <ul>
+	            <li><a href="accueil_FR.php">FR |</a></li>
+	            <li><a href="accueil_NL.php">NL |</a></li>
+	            <li><a href="accueil_EN.php">EN </a></li>
+	        </ul>
+	    </nav>
+    </footer>
 </body>
