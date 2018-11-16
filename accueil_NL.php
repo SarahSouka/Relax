580e458a51297de442148f6d2c914a6537707743
Cindy Colin <cindycolin@outlook.be>
2018-11-16 11:22:00 +0100

    footer Lang spaces

--- accueil_NL.php
+++ accueil_NL.php
@@ -167,7 +167,7 @@
 	<p> &copy; Bénédicte Regnier, Floriane Jacques, Sarah Gueye, Cindy Colin - Interface 3</p>
 	  <nav>
 	        <ul>
-	           <li><a href="accueil_FR.php"> FR |</a></li>
+	            <li><a href="accueil_FR.php"> FR |</a></li>
 	            <li><a href="accueil_NL.php"> NL |</a></li>
 	            <li><a href="accueil_EN.php"> EN </a></li>
 	        </ul>
