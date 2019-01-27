fd9bbbb45913b12b5515248fc90d7a641d70f9b1
unknown <fjacques.ulb@gmail.com>
2019-01-27 20:40:00 +0100

    modifs sur la nav, le footer et ajout d'une marge dans la page résultat

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
 <footer>
-	    <p> &copy; Bénédicte Regnier, Floriane Jacques, Sarah Gueye, Cindy Colin - Interface3</p>
+	    <p> &copy; Bénédicte Regnier, Floriane Jacques, Sarah Gueye, Cindy Colin - Travail de groupe réalisé dans le cadre de la formation Front-end developer 2018 - 2019 à Interface3</p>
 	    <nav>
 	        <ul>
-	            <li><a href="accueil_FR.php"> FR |</a></li>
-	            <li><a href="accueil_NL.php"> NL |</a></li>
+	            <li><a class="borderright" href="accueil_FR.php"> FR </a></li>
+	            <li><a class="borderright" href="accueil_NL.php"> NL </a></li>
 	            <li><a href="accueil_EN.php"> EN </a></li>
 	        </ul>
 	    </nav>
